74ae8037ea680ce31fd955d9722aca0d10554812
mattperkins <github@mattperkins.com>
2018-11-10 22:15:17 +0000

    update

--- page.php
+++ page.php
@@ -1,22 +1,24 @@
 <?php
 get_header();
 
+// get individual page content from WP admin
 while(have_posts()) {
  the_post(); 
 ?>
 
+<!-- WP title -->
 <h1><?php
 the_title();
 ?></h1>
 
+<!-- WP content -->
 <div class="well">
 <?php
 the_content();
 ?>
 </div>
 
+<!-- WP footer -->
 <?php }
-
 get_footer();
-
 ?>
