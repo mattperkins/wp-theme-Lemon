a0d7438c5b481e5d19f1e80ce61cd07069612c69
mattperkins <github@mattperkins.com>
2018-11-10 22:29:09 +0000

    remove static title meta

--- header.php
+++ header.php
@@ -4,8 +4,7 @@
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <meta http-equiv="X-UA-Compatible" content="ie=edge">
- <title>Lemon Theme</title>
- <!-- include functions.php => styles -->
+ <!-- include functions.php == title & styles meta -->
  <?php
    wp_head();
  ?>
