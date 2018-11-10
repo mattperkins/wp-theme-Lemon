ec8376a0b7b12e1886e4a93c3dd1b0565238d676
mattperkins <github@mattperkins.com>
2018-11-10 23:38:05 +0000

    add home link

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <!-- navigation -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   
-  <a class="navbar-brand" href="#"><?php bloginfo('title'); ?></a>
+  <a class="navbar-brand" href="<?php echo site_url('/') ?>"><?php bloginfo('title'); ?></a>
   
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
