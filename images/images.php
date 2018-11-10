be9e409080e69cce5706480cd31663bf8b932fe7
mattperkins <github@mattperkins.com>
2018-11-10 21:23:46 +0000

    init

---
+++ images/images.php
@@ -0,0 +1,7 @@
+<!-- absolute path to images -->
+<!-- /wp-content/themes/lemon/images/image.jpg -->
+
+<!-- can be shortened using WP function to: -->
+<?php
+  echo get_theme_file_uri('images/image.jpg')
+?>
