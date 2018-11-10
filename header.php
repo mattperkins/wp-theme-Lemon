88a6f2be411670b739edc0027285315f6202999d
mattperkins <github@mattperkins.com>
2018-11-10 21:23:31 +0000

    init

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <title>Lemon Theme</title>
+ <!-- include functions.php => styles -->
+ <?php
+   wp_head();
+ ?>
+</head>
+<body>
+
+<!-- bootstrap main container -->
+ <div class="container">
