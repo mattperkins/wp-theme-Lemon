157c93589cbbf3259a0642b1493a74a25a9b0218
mattperkins <github@mattperkins.com>
2018-11-11 00:29:37 +0000

    add custom class to button removing outline

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 console.log('connected');
+
+$(document).ready(function () {
+  //your code here
+
+// custom hamburger navigation
+
+ $(document).ready(function(){
+     $('#nav-mhweb-hamburger').click(function(){
+     $(this).toggleClass('open');
+   });
+ });
+
+});
