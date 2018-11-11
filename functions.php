3e19f55ccfbac24ccd0fed1b1dce8906f2bcb4ae
mattperkins <github@mattperkins.com>
2018-11-11 00:28:45 +0000

    correct the order of script cascade

--- functions.php
+++ functions.php
@@ -1,14 +1,17 @@
 <?php
 
 function lemon_files(){
+
+// bootstrap jquery
+  wp_enqueue_script('bs-jquery', ('//code.jquery.com/jquery-3.2.1.slim.min.js'), NULL, '1.0', true);
+  // bootstrap popper
+  wp_enqueue_script('bs-popper', ('//cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'), NULL, '1.0', true);
+  // bootstrap bundle
+  wp_enqueue_script('bs-js', ('//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'), NULL, '1.0', true);
+
 // main website javascript file
 // filename, dependencies(none = NULL), version number, load file before closing body tag, yes=true/no=false
    wp_enqueue_script('lemon-js', get_theme_file_uri('/js/index.js'), NULL, '1.0', true);
-
-// bootstrap js
-  wp_enqueue_script('bs-jquery', ('//code.jquery.com/jquery-3.2.1.slim.min.js'), NULL, '1.0', true);
-  wp_enqueue_script('bs-popper', ('//cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'), NULL, '1.0', true);
-  wp_enqueue_script('bs-js', ('//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'), NULL, '1.0', true);
 
 // bootstrap css
    wp_enqueue_style('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
